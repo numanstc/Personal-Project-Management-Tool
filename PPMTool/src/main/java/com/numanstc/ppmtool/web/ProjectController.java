1fa2988a90d385616ac0582d97212ff41950bff8
Numan <numan_satici@hotmail.com>
2020-03-31 05:21:02 +0300

    Refactoring Validation

--- PPMTool/src/main/java/com/numanstc/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/numanstc/ppmtool/web/ProjectController.java
@@ -1,45 +1,36 @@
 package com.numanstc.ppmtool.web;
 
 import com.numanstc.ppmtool.domain.Project;
+import com.numanstc.ppmtool.services.MapValidationErrorService;
 import com.numanstc.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final MapValidationErrorService mapValidationErrorService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, MapValidationErrorService mapValidationErrorService) {
         this.projectService = projectService;
+        this.mapValidationErrorService = mapValidationErrorService;
     }
 
     @PostMapping
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-//        Binding Error hataları barındıran bir interface. hataları bu şekilde yönetebilirz.
-        if (result.hasErrors()) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
-            Map<String, String> errorMap  = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
-//            return new ResponseEntity<>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
-        }
         Project savedProject = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
     }
