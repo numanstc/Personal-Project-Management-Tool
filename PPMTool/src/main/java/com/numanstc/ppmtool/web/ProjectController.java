7d67c27a032e20835a7ebab9bcec545f61e384be
Numan <numan_satici@hotmail.com>
2020-03-31 04:52:10 +0300

    Valditaion Hatalarını daha sade hale ggetirdik.

--- PPMTool/src/main/java/com/numanstc/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/numanstc/ppmtool/web/ProjectController.java
@@ -5,12 +5,15 @@ import com.numanstc.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -27,7 +30,15 @@ public class ProjectController {
 
 //        Binding Error hataları barındıran bir interface. hataları bu şekilde yönetebilirz.
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid Object", HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap  = new HashMap<>();
+
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+//            return new ResponseEntity<>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
         }
         Project savedProject = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
