b1ef589e83a2b9d2170ec841e3384a6665fb1d2e
Numan <numan_satici@hotmail.com>
2020-04-05 02:24:13 +0300

    redux chrome eklentisiyle birlikte bassit ayarları yapıldı

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,19 +3,23 @@ import Dashboard from "./componenets/Dashboard";
 import Header from "./componenets/Layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./componenets/Project/AddProject";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
