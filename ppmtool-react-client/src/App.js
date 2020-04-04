c8a8a17608d474437dd153dba8b021a92f68a564
Numan <numan_satici@hotmail.com>
2020-04-04 17:56:07 +0300

    added jsx pages' styles

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Dashboard from "./componenets/Dashboard";
 import Header from "./componenets/Layout/Header";
+
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
       <Header />
+      <Dashboard />
     </div>
   );
 }
