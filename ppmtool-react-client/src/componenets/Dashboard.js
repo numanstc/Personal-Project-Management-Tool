7bde3f656d5416c0e40e407543af8998e28dbcbc
Numan <numan_satici@hotmail.com>
2020-04-04 17:30:53 +0300

    added bootstrap

---
+++ ppmtool-react-client/src/componenets/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjecItem";
+
+class Dashbord extends Component {
+  render() {
+    return (
+      <div>
+        <h1> This is Dashboard </h1>
+        <ProjectItem />
+      </div>
+    );
+  }
+}
+export default Dashbord;
---
+++ ppmtool-react-client/src/componenets/Project/ProjecItem.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class ProjectItem extends Component {
+  render() {
+    return <h1> This is ProjectItem </h1>;
+  }
+}
+export default ProjectItem;
