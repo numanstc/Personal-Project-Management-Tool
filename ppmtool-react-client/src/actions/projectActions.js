7f1242592ddebf220a241e5d5ae732a4a2cbdf95
Numan <numan_satici@hotmail.com>
2020-04-09 19:40:58 +0300

    Delete Proje Eklendi.

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 // Burası api isteklerinin yapıldığı yer
 
@@ -46,4 +46,12 @@ export const getProject = (id, history) => async dispatch => {
       // eğer istenilen adres yoksa yönlendirme yapıyor.
       history.push("/dashboard");
     });
+};
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
