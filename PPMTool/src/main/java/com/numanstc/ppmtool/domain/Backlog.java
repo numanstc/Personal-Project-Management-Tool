59c28d212d5aaf8ce69adcb2d7fdbcb87d16b6f8
Numan <numan_satici@hotmail.com>
2020-04-10 01:56:41 +0300

    Backlog ve ProjectTask oneToMany ilişkisi eklendi.

--- PPMTool/src/main/java/com/numanstc/ppmtool/domain/Backlog.java
+++ PPMTool/src/main/java/com/numanstc/ppmtool/domain/Backlog.java
@@ -3,6 +3,8 @@ package com.numanstc.ppmtool.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -20,9 +22,9 @@ public class Backlog {
     @JsonIgnore
     private Project project;
 
-
-    //oneToMany projectTask
-
+    //OneToMany projectTask
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
     public Backlog() {
     }
@@ -57,5 +59,13 @@ public class Backlog {
 
     public void setProject(Project project) {
         this.project = project;
+    }
+
+    public List<ProjectTask> getProjectTasks() {
+        return projectTasks;
+    }
+
+    public void setProjectTasks(List<ProjectTask> projectTasks) {
+        this.projectTasks = projectTasks;
     }
 }
--- PPMTool/src/main/java/com/numanstc/ppmtool/domain/ProjectTask.java
+++ PPMTool/src/main/java/com/numanstc/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,7 @@
 package com.numanstc.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -19,6 +21,10 @@ public class ProjectTask {
     private Integer priority;
     private Date dueDate;
     // ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH) //bu objeyi sildiğimde backlog resflesh etsin
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
 
     @Column(updatable = false)
     private String projectIdentifier;
@@ -107,6 +113,14 @@ public class ProjectTask {
 
     public void setUpdated_At(Date updated_At) {
         this.updated_At = updated_At;
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @PrePersist
