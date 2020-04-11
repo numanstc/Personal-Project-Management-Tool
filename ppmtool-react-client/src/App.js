cf55f886828ecd1f6ed358159c1cd3b61d6aae29
Numan <numan_satici@hotmail.com>
2020-04-12 00:43:11 +0300

    ProjectBoard ve AddProjectTask routları eklendi

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,8 +4,10 @@ import Header from "./componenets/Layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./componenets/Project/AddProject";
 import UpdateProject from "./componenets/Project/UpdateProject";
+import ProjectBoard from "./componenets/ProjectBoard/ProjectBoard";
 import { Provider } from "react-redux";
 import store from "./store";
+import AddProjectTask from "./componenets/ProjectBoard/ProjectTasks/AddProjectTask";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -19,6 +21,8 @@ function App() {
           <Route path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
         </div>
       </Router>
     </Provider>
