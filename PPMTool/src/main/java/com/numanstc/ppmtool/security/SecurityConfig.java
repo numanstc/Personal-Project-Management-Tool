cc1b996f2cd80080a4a5ea394ddf221a5f6931a2
Numan <numan_satici@hotmail.com>
2020-04-16 01:32:28 +0300

    Authorize Error Page ve Authentication'ın temel ayarları yapıldı ve bazı açıklamalar eklendi

---
+++ PPMTool/src/main/java/com/numanstc/ppmtool/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.numanstc.ppmtool.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        Cross-Origin Resource Sharing
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //stateless rest api için jwt sessionlarını tut
+                .and()
+                .headers().frameOptions().sameOrigin() // to enable H2 db
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                        ).permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
