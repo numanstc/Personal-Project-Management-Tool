0e272afe7fde256c9da938e616e25fd34e5df48c
Numan <numan_satici@hotmail.com>
2020-04-16 02:50:06 +0300

    User sınıfı UserDetailService sınıfını implement etti createUser servisi ve buna ilişkin apilar oluşturuldu

--- PPMTool/src/main/java/com/numanstc/ppmtool/PpmtoolApplication.java
+++ PPMTool/src/main/java/com/numanstc/ppmtool/PpmtoolApplication.java
@@ -2,6 +2,8 @@ package com.numanstc.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PpmtoolApplication {
@@ -10,4 +12,8 @@ public class PpmtoolApplication {
         SpringApplication.run(PpmtoolApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- PPMTool/src/main/java/com/numanstc/ppmtool/security/UserService.java
+++ PPMTool/src/main/java/com/numanstc/ppmtool/security/UserService.java
@@ -1,7 +1,9 @@
 package com.numanstc.ppmtool.security;
 
+import com.numanstc.ppmtool.domain.User;
 import com.numanstc.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,17 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        // username has to be unique (exception)
+
+        // make sure password and confirmPassword match
+        // we don't persist or show confirmPassword
+        return userRepository.save(newUser);
+    }
 }
