08bf4d779e2fad4fd1595ea334e54e5a24c91ea7
Numan <numan_satici@hotmail.com>
2020-04-18 23:26:37 +0300

    LoginReducer ve login action'ı eklendi

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJsonWebToken from "../securityUtils/setJsonWebToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatct => {
   await axios
@@ -9,6 +11,34 @@ export const createNewUser = (newUser, history) => async dispatct => {
       dispatct({
         type: GET_ERRORS,
         payload: {}
+      });
+    })
+    .catch(err => {
+      dispatct({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+export const login = LoginRequest => async dispatct => {
+  // post => Login request
+  await axios
+    .post("/api/users/login", LoginRequest)
+    .then(res => {
+      // extract token from res.token
+      const { token } = res.data;
+      // store the token in the local store
+      localStorage.setItem("jwtToken", token);
+      // set our token in the header
+      setJsonWebToken(token);
+      // decode token on React
+      const decoded = jwt_decode(token);
+      // dispatch to our securityReducer
+
+      dispatct({
+        type: SET_CURRENT_USER,
+        payload: decoded
       });
     })
     .catch(err => {
