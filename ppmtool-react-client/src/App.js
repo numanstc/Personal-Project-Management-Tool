670e51a21bb120e8793d9768c49ef768406823f0
Numan <numan_satici@hotmail.com>
2020-04-18 14:27:27 +0300

    Login ve register sayfaları ve bunların routeları eklendi

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -9,6 +9,9 @@ import { Provider } from "react-redux";
 import store from "./store";
 import AddProjectTask from "./componenets/ProjectBoard/ProjectTasks/AddProjectTask";
 import UpdateProjectTask from "./componenets/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./componenets/Layout/Landing";
+import Register from "./componenets/UserManagement/Register";
+import Login from "./componenets/UserManagement/Login";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,9 +19,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <Provider store={store}>
+      {/*public global redux database i */}
+
       <Router>
         <div className="App">
           <Header />
+
+          {/*public Route*/}
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+          {/*private Roues*/}
           <Route path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
